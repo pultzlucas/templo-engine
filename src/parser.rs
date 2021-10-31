33d9c7ac8f39673931c15c3949ff9a333580d3f0
Lucas-Lopes-Pultz <pultzlucas@gmail.com>
2021-10-30 23:08:11 -0300

    clean code

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,36 @@
-use regex::Regex;
+use crate::{
+    syntax_tree::{SyntaxTree, TreeType},
+    token::{Token, TokenType},
+};
 
-use crate::token::Token;
-// use std::io::Error;
+pub fn parse(tokens: Vec<Token>) -> SyntaxTree {
+    let mut tokens_iter = tokens.iter();
+    let mut tree: SyntaxTree = SyntaxTree::new(
+        tokens_iter.next().unwrap().value.clone(),
+        TreeType::FunctionCall,
+        vec![],
+    );
+    let mut n_of_args = 0;
 
-pub fn parse(tokens: &Vec<Token>) {
-    
+    for token in tokens_iter.rev() {
+        if token.type_ == TokenType::BracketRight {
+            n_of_args = 0;
+        }
+
+        if token.type_ == TokenType::Function {
+            n_of_args = 1;
+            let mut args: Vec<SyntaxTree> = tree.get_child(n_of_args..);
+            args.reverse();
+            let fun = SyntaxTree::new(token.value.clone(), TreeType::FunctionCall, args);
+            tree.append_child(fun);
+        }
+
+        if token.type_ == TokenType::Input {
+            n_of_args += 1;
+            let input = SyntaxTree::new(token.value.clone(), TreeType::Input, vec![]);
+            tree.append_child(input);
+        }
+    }
+    tree.childs.reverse();
+    tree
 }
--- src/syntax_tree.rs
+++ src/syntax_tree.rs
@@ -1,17 +1,16 @@
-use crate::token::{Token, TokenType};
+use std::{collections::btree_map::Range, ops::RangeFrom};
 
 #[derive(Debug, Clone)]
 pub enum TreeType {
     FunctionCall,
     Input,
-    Root,
 }
 
 #[derive(Debug, Clone)]
 pub struct SyntaxTree {
     node: String,
     tree_type: TreeType,
-    childs: Vec<SyntaxTree>,
+    pub childs: Vec<SyntaxTree>,
 }
 
 impl SyntaxTree {
@@ -27,43 +26,8 @@ impl SyntaxTree {
         self.childs.push(child);
         self
     }
-}
 
-// I -> a-z|A-Z|0-9
-// Arg -> Fn|I|Fn,|I,
-// Fn -> I(Arg*)
-
-pub fn build_tree(tokens: Vec<Token>) {
-    let mut tokens_iter = tokens.iter();
-    let root = tokens_iter.next().unwrap();
-    let mut tree: SyntaxTree = SyntaxTree::new(root.value.clone(), TreeType::FunctionCall, vec![]);
-
-    let mut n_of_args = 0;
-    let mut _memory: Box<SyntaxTree>;
-
-    for (_, token) in tokens_iter.rev().enumerate() {
-        if token.type_ == TokenType::BracketRight {
-            n_of_args = 0;
-        }
-
-        if token.type_ == TokenType::Function {
-            n_of_args = 1;
-            // println!("----- {} -----", token.value);
-            // println!("range -> {:?}", n_of_args..);
-            // println!("{:?}\n", tree.childs);
-            let mut args: Vec<SyntaxTree> = tree.childs.splice(n_of_args.., []).collect();
-            args.reverse();
-            let fun = SyntaxTree::new(token.value.clone(), TreeType::FunctionCall, args);
-            tree.append_child(fun);
-            println!("{:?}", tree.childs);
-        }
-
-        if token.type_ == TokenType::Input {
-            n_of_args += 1;
-            let input = SyntaxTree::new(token.value.clone(), TreeType::Input, vec![]);
-            tree.append_child(input);
-        }
+    pub fn get_child(&mut self, range: RangeFrom<usize>) -> Vec<SyntaxTree> {
+        self.childs.splice(range, []).collect()
     }
-    tree.childs.reverse();
-    println!("####\n{:?}\n", tree);
 }
